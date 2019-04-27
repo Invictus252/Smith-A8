e40d2abe5d40a35bc8909201ff5aa9c7f52f91ed
Jason Smith <invictus252@gmail.com>
2019-04-27 14:02:19 +0000

    completed

---
+++ server.js
@@ -0,0 +1,258 @@
+const express = require('express');
+const app = express();
+
+// added so we can serve index.html...
+var http = require('http');
+var fs = require('fs');
+
+var mysql = require('mysql');
+
+// install first using npm install bcrypt
+const bcrypt = require('bcrypt');
+
+const conInfo = 
+{
+    host: process.env.IP,
+    user: process.env.C9_USER,
+    password: "",
+    database: "THUNTDB"
+};
+
+var session = require('express-session'); 
+app.use(session({ secret: 'happy jungle', 
+                  resave: false, 
+                  saveUninitialized: false, 
+                  cookie: { maxAge: 600000 }}))
+
+app.all('/', serveIndex);                  
+app.all('/whoIsLoggedIn', whoIsLoggedIn);                  
+app.all('/register', register);
+app.all('/login', login);
+app.all('/logout', logout);
+app.all('/updateLow', updateLow);
+app.all('/resetLow', resetLow);
+
+
+
+app.listen(process.env.PORT,  process.env.IP, startHandler())
+
+function startHandler()
+{
+  console.log('Server listening on port ' + process.env.PORT)
+}
+
+
+function updateLow(req, res)
+{
+  var con = mysql.createConnection(conInfo);
+  
+  console.log("@updateLow");
+  let sql = `UPDATE USER
+           SET USER_LOW = ?
+           WHERE USER_ID = ?`;
+ 
+  let data = [req.query.low, req.session.user.result.id];
+  
+  
+ 
+  if(req.query.low  <  req.session.user.result.low || isNaN(req.session.user.result.low)){
+      con.query(sql, data, (error, results, fields) => {
+      if (error){
+        return console.error(error.message);
+      }
+      else
+      {
+        con.query("SELECT * FROM USER WHERE USER_EMAIL = ?", [req.session.user.result.email], function (err, result, fields) 
+        {
+          if (err) 
+            writeResult(req, res, {'error' : err});
+          else
+          {
+            req.session.user = {'result' : {'id': result[0].USER_ID, 'email': result[0].USER_EMAIL, 'low': result[0].USER_LOW}};
+            writeResult(req, res, req.session.user);
+          }
+        });
+      }
+      console.log('Rows affected:', results.affectedRows);
+    });
+  }
+}
+
+function resetLow(req, res)
+{
+  var con = mysql.createConnection(conInfo);
+  
+  console.log("@resetLow");
+  let sql = `UPDATE USER
+           SET USER_LOW = ?
+           WHERE USER_ID = ?`;
+ 
+  let data = [req.query.low, req.session.user.result.id];
+  con.query(sql, data, (error, results, fields) => {
+  if (error){
+    return console.error(error.message);
+    }
+  else
+  {
+    con.query("SELECT * FROM USER WHERE USER_EMAIL = ?", [req.session.user.result.email], function (err, result, fields) 
+    {
+    if (err) 
+      writeResult(req, res, {'error' : err});
+    else
+    {
+      req.session.user = {'result' : {'id': result[0].USER_ID, 'email': result[0].USER_EMAIL, 'low': result[0].USER_LOW}};
+      writeResult(req, res, req.session.user);
+    }
+    });
+  }
+  console.log('Rows affected:', results.affectedRows);
+  });
+}
+
+function whoIsLoggedIn(req, res)
+{
+  if (req.session.user == undefined)
+    writeResult(req, res, {'error' : 'Nobody is logged in.'});
+  else
+    writeResult(req, res, req.session.user);
+}
+
+function register(req, res)
+{
+  if (req.query.email == undefined || !validateEmail(req.query.email))
+  {
+    writeResult(req, res, {'error' : "Please specify a valid email"});
+    return;
+  }
+
+  if (req.query.password == undefined || !validatePassword(req.query.password))
+  {
+    writeResult(req, res, {'error' : "Password must have a minimum of eight characters, at least one letter and one number"});
+    return;
+  }
+
+  var con = mysql.createConnection(conInfo);
+  con.connect(function(err) 
+  {
+    if (err) 
+      writeResult(req, res, {'error' : err});
+    else
+    {
+      // bcrypt uses random salt is effective for fighting
+      // rainbow tables, and the cost factor slows down the
+      // algorithm which neutralizes brute force attacks ...
+      let hash = bcrypt.hashSync(req.query.password, 12);
+      con.query("INSERT INTO USER (USER_EMAIL, USER_PASS, USER_LOW) VALUES (?, ?,?)", [req.query.email, hash,10], function (err, result, fields) 
+      {
+        if (err) 
+        {
+          if (err.code == "ER_DUP_ENTRY")
+            err = "User account already exists.";
+          writeResult(req, res, {'error' : err});
+        }
+        else
+        {
+          con.query("SELECT * FROM USER WHERE USER_EMAIL = ?", [req.query.email], function (err, result, fields) 
+          {
+            if (err) 
+              writeResult(req, res, {'error' : err});
+            else
+            {
+              req.session.user = {'result' : {'id': result[0].USER_ID, 'email': result[0].USER_EMAIL, 'low': result[0].USER_LOW}};
+              writeResult(req, res, req.session.user);
+            }
+          });
+        }
+      });
+    }
+  });
+  
+}
+
+function login(req, res)
+{
+  if (req.query.email == undefined)
+  {
+    writeResult(req, res, {'error' : "Email is required"});
+    return;
+  }
+
+  if (req.query.password == undefined)
+  {
+    writeResult(req, res, {'error' : "Password is required"});
+    return;
+  }
+  
+  var con = mysql.createConnection(conInfo);
+  con.connect(function(err) 
+  {
+    if (err) 
+      writeResult(req, res, {'error' : err});
+    else
+    {
+      con.query("SELECT * FROM USER WHERE USER_EMAIL = ?", [req.query.email], function (err, result, fields) 
+      {
+        if (err) 
+          writeResult(req, res, {'error' : err});
+        else
+        {
+          if(result.length == 1 && bcrypt.compareSync(req.query.password, result[0].USER_PASS))
+          {
+            req.session.user = {'result' : {'id': result[0].USER_ID, 'email': result[0].USER_EMAIL, 'low': result[0].USER_LOW}};
+            writeResult(req, res, req.session.user);
+          }
+          else 
+          {
+            writeResult(req, res, {'error': "Invalid email/password"});
+          }
+        }
+      });
+    }
+  });
+}
+
+function logout(req, res)
+{
+  req.session.user = undefined;
+  writeResult(req, res, {'error' : 'Nobody is logged in.'});
+}
+
+function writeResult(req, res, obj)
+{
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify(obj));
+  res.end('');
+}
+
+function validateEmail(email) 
+{
+  if (email == undefined)
+  {
+    return false;
+  }
+  else
+  {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+}
+
+function validatePassword(pass)
+{
+  if (pass == undefined)
+  {
+    return false;
+  }
+  else
+  {
+    var re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return re.test(pass);
+  }
+}
+
+function serveIndex(req, res)
+{
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var index = fs.readFileSync('index.html');
+  res.end(index);
+}
